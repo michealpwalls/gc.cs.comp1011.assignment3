7277d4a51f8a7ad99c17c822ac0d73bbb36cd6a4
Micheal Walls <michealpwalls@gmail.com>
2013-12-06 13:48:57 -0500

    Event comes alive!
    IT'S ALIIIIIIVE! :)

--- src/gc/cs/comp1011/assignment3/CardGame.java
+++ src/gc/cs/comp1011/assignment3/CardGame.java
@@ -22,6 +22,7 @@ public class CardGame extends JFrame {
 	
 	// Instance Variables
 	private static final long serialVersionUID = -3755250864096119104L;
+	private Card previousCard;
 
 	/**
 	 * This is the default, no-argument Constructor for CardGame objects
@@ -30,9 +31,20 @@ public class CardGame extends JFrame {
 	 */
 	public CardGame() throws IOException {
 		super();
+		
+		// Initially, set the previousCard to null
+		previousCard = null;
+		
 		setLayout( new GridLayout(4,4) );
 		
-		Card testCard = new Card("13","h");
+		@SuppressWarnings("unused")
+		Card testCard1 = new Card(13,'h');
+		@SuppressWarnings("unused")
+		Card testCard2 = new Card(12,'c');
+		@SuppressWarnings("unused")
+		Card testCard3 = new Card(10,'d');
+		@SuppressWarnings("unused")
+		Card testCard4 = new Card(11,'s');
 	}// end CardGame Constructor
 	
 	/**
@@ -43,12 +55,19 @@ public class CardGame extends JFrame {
 	public class Card {
 		// Instance variables
 		private JButton cardButton;
+		private int cardValue;
+		private char cardSuit;
+		private String cardTitle;
 		
-		public Card(String cardValue, String cardSuit) throws IOException {
+		public Card(int cardValueIn, char cardSuitIn) throws IOException {
+			// Store the card's properties
+			this.cardValue = cardValueIn;
+			this.cardSuit = cardSuitIn;
+			this.cardTitle = String.valueOf( cardValue ) +  String.valueOf( cardSuit );
 			
-			Icon cardIcon = new ImageIcon(ImageIO.read(new File("res/images/card_" + cardValue + cardSuit + ".png")));
+			Icon cardIcon = new ImageIcon(ImageIO.read(new File("res/images/card_" + this.cardValue + this.cardSuit + ".png")));
 			
-			cardButton = new JButton(cardValue, cardIcon);
+			cardButton = new JButton(cardIcon);
 			//fancyButton.setRolloverIcon(mpwIcon2);
 			add(cardButton);
 			
@@ -56,14 +75,30 @@ public class CardGame extends JFrame {
 			cardButton.addActionListener(clickHandler);
 		}// end Card Constructor
 		
+		public boolean Compare(Card cardToCompare) {
+			//
+			// TODO: Compare the two card objects
+			//
+			return false;
+			
+		}// end Compare method
+		
 		/**
 		 * Inner class implementing a handler for OnClick events
 		 */
 		private class CardClickHandler implements ActionListener {
 
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				JOptionPane.showMessageDialog( CardGame.this, String.format("You pressed %s", arg0.getActionCommand()) );
+			public void actionPerformed(ActionEvent e) {
+				//
+				//TODO Something very profound and awesome
+				//
+				if( previousCard == null ) {
+					JOptionPane.showMessageDialog( CardGame.this, String.format("You pressed the FIRST card!") );
+					previousCard = Card.this;
+				} else {
+					JOptionPane.showMessageDialog( CardGame.this, String.format("You pressed a SECOND card.\nCompare this card to " + previousCard.cardTitle));
+				}
 			}// end actionPerformed method
 			
 		}// end CardClickHandler class
