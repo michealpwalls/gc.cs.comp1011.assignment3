52f8284ebef5ca825acdf88a7abd0ef4a56223df
Micheal Walls <michealpwalls@gmail.com>
2013-12-06 16:50:31 -0500

    Milestone: Random
    Random generation of 8 *unique* cards out of the set of 52 possible
    cards!

--- src/gc/cs/comp1011/assignment3/CardGame.java
+++ src/gc/cs/comp1011/assignment3/CardGame.java
@@ -3,17 +3,17 @@ package gc.cs.comp1011.assignment3;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+
+import java.util.Random;
 
 /**
  * @author Micheal Walls
@@ -24,6 +24,20 @@ public class CardGame extends JFrame {
 	// Instance Variables
 	private static final long serialVersionUID = -3755250864096119104L;	// Required by JFrame, allows serializing of the Object
 	private Card previousCard;	// Reserve area in memory for a Card object
+	private int[] array_selectedCards = new int[8];
+	private final String[] array_cardIdentifiers = {
+			"card_1c.png", "card_1d.png", "card_1h.png", "card_1s.png", 
+		   "card_2c.png", "card_2d.png", "card_2h.png", "card_2s.png", "card_3c.png", 
+		   "card_3d.png", "card_3h.png", "card_3s.png", "card_4c.png", "card_4d.png", 
+		   "card_4h.png", "card_4s.png", "card_5c.png", "card_5d.png", "card_5h.png", 
+		   "card_5s.png", "card_6c.png", "card_6d.png", "card_6h.png", "card_6s.png", 
+		   "card_7c.png", "card_7d.png", "card_7h.png", "card_7s.png", "card_8c.png", 
+		   "card_8d.png", "card_8h.png", "card_8s.png", "card_9c.png", "card_9d.png", 
+		   "card_9h.png", "card_9s.png", "card_10c.png", "card_10d.png", "card_10h.png", 
+		   "card_10s.png", "card_11c.png", "card_11d.png", "card_11h.png", "card_11s.png", 
+		   "card_12c.png", "card_12d.png", "card_12h.png", "card_12s.png", "card_13c.png", 
+		   "card_13d.png", "card_13h.png", "card_13s.png"
+	};
 
 	/**
 	 * This is the default, no-argument Constructor for CardGame objects
@@ -31,23 +45,43 @@ public class CardGame extends JFrame {
 	 * @throws IOException Throws when the card image could not be found
 	 */
 	public CardGame() throws IOException {
+		// Call the super/parent/base Class' constructor
 		super();
 		
 		// Initially, set the previousCard to null
 		previousCard = null;
-		
+
 		// This instantiates an instance of our JFrame with a 4 x 4 Grid Layout
 		setLayout( new GridLayout(4,4) );
 		
-		// We instantiate our cards but the compiler warns we never use them. Just suppress it :P
-		@SuppressWarnings("unused")
-		Card testCard1 = new Card(13,'h');
-		@SuppressWarnings("unused")
-		Card testCard2 = new Card(12,'c');
-		@SuppressWarnings("unused")
-		Card testCard3 = new Card(10,'d');
-		@SuppressWarnings("unused")
-		Card testCard4 = new Card(11,'s');
+		// Instantiate an instance of the Random object
+		Random randomCard = new Random();
+		int nextRandom = 0;
+		
+		for( int i = 0; i < 8; i++ ) {
+			// A flag for the while loop
+			boolean uniqueCardFound = true;
+
+			while( uniqueCardFound ) {
+				// Generate a random integer
+				nextRandom = randomCard.nextInt(51);
+
+				// Make sure this card hasn't already been chosen
+				for( int y = 0; y < array_selectedCards.length; y++ ) {
+
+					if ( array_selectedCards[y] != nextRandom ) {
+						uniqueCardFound = false;
+					}// end if
+
+				}// end inner for loop
+
+			}// end while loop
+
+			// The compiler warns we never use them. Just suppress it :P
+			@SuppressWarnings("unused")
+			Card testCard1 = new Card( array_cardIdentifiers[nextRandom] );
+		}// end outer for loop
+
 	}// end CardGame Constructor
 	
 	/**
@@ -61,9 +95,7 @@ public class CardGame extends JFrame {
 	public class Card {
 		// Instance variables
 		private JButton cardButton;
-		private int cardValue;
-		private char cardSuit;
-		private String cardTitle;
+		private String cardIdentifier;
 		
 		/**
 		 * Constructor for instances of Card objects
@@ -72,13 +104,11 @@ public class CardGame extends JFrame {
 		 * @param cardSuitIn Card Suit as a single character (h:hearts)
 		 * @throws IOException Throws when the card image could not be found
 		 */
-		public Card(int cardValueIn, char cardSuitIn) throws IOException {
+		public Card(String cardIdentifier) throws IOException {
 			// Store the card's properties
-			this.cardValue = cardValueIn;
-			this.cardSuit = cardSuitIn;
-			this.cardTitle = String.valueOf( cardValue ) +  String.valueOf( cardSuit );
+			this.cardIdentifier = cardIdentifier;
 			
-			Icon cardIcon = new ImageIcon(ImageIO.read(new File("res/images/card_" + this.cardValue + this.cardSuit + ".png")));
+			Icon cardIcon = new ImageIcon( ImageIO.read(new File("res/images/" + this.cardIdentifier)) );
 			
 			cardButton = new JButton(cardIcon);
 			add(cardButton);
@@ -127,7 +157,7 @@ public class CardGame extends JFrame {
 					//
 					// DEBUG: Show that the Second card was clicked. Remove later on...
 					//
-					JOptionPane.showMessageDialog( CardGame.this, String.format("You pressed a SECOND card.\nCompare this card to " + previousCard.cardTitle));
+					JOptionPane.showMessageDialog( CardGame.this, String.format("You pressed a SECOND card.\nCompare this card to " + previousCard.cardIdentifier));
 					//
 					// TODO: Flip the card.
 					//
